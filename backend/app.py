3abf7e0cd658d5e6290d102366b60a70cf16e7aa
root <root@Shahzaib>
2024-01-29 00:00:09 +0500

    added the web sockets

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 from typing import List
 from fastapi.middleware.cors import CORSMiddleware
@@ -29,18 +29,53 @@ chats = []
 async def read_tasks():
     return chats
 
-@app.post("/chats/")
-async def create_response(question: Chat):
+class ConnectionManager:
+    """Class defining socket events"""
+    def __init__(self):
+        """init method, keeping track of connections"""
+        self.active_connections = []
+    
+    async def connect(self, websocket: WebSocket):
+        """connect event"""
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        """Direct Message"""
+        await websocket.send_text(message)
+    
+    def disconnect(self, websocket: WebSocket):
+        """disconnect event"""
+        self.active_connections.remove(websocket)
+
+manager = ConnectionManager()
+
+@app.websocket("/chats")
+async def create_response(websocket: WebSocket):
+    await manager.connect(websocket)
     prompt = ChatPromptTemplate.from_template("Answer the following question to the best of your ability: {question}")
     model = ChatOpenAI()
     output_parser = StrOutputParser()
     chain = prompt | model | output_parser
 
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            async for chunk in chain.astream({"question": data}):
+                await manager.send_personal_message(chunk,websocket)
+    except websockets.exceptions.WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.send_personal_message("Bye!!!",websocket)
+
+
+
+
+
     # response = chain.invoke({"question": question})
     # print(response)
     # return response
-    async for chunk in chain.astream({"question": question}):
-        print(chunk)
+
 
 
 
